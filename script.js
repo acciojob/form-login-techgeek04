6cc033ea0f34c18dd83ad62bebf30f42d8b3f714
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-08-22 20:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
+    var form = document.getElementById('form1');
 
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
+    
+    alert(firstName +" "+ lastName);
 }
